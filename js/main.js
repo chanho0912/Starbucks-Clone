6d20c4682476e02a3816360b6c078afe36fe5a3a
chanho0912 <chanho0912@naver.com>
2021-05-14 15:40:21 +0900

    HeaderBadge finish

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 }); 
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if(window.scrollY) {
+    // badge hidden
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+  else {
+    // badge show
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+
+// _.throttle(function, time)

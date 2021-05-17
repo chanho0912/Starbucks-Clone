650b8e4decf85820aab89736c7bbe0ef28207eaf
chanho0912 <chanho0912@naver.com>
2021-05-17 22:29:52 +0900

    Swiper, YoutubeIframeAPI

--- js/main.js
+++ js/main.js
@@ -37,3 +37,54 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 
 // _.throttle(function, time)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 5000
+  // }
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl : '.promotion .swiper-prev',
+    nextEl : '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion){
+    // Hide logic
+    promotionEl.classList.add('hide');
+  }
+  else {
+    promotionEl.classList.remove('hide');
+  }
+});
+
+function floatingObject (selector) {
+  // gsap.to();
+}

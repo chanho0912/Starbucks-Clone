370bfeeb1f23757cf312e8cf6e8fa539549ebec7
chanho0912 <chanho0912@naver.com>
2021-05-17 23:15:45 +0900

    animation updated

--- js/main.js
+++ js/main.js
@@ -85,6 +85,22 @@ promotionToggleBtn.addEventListener('click', function () {
   }
 });
 
-function floatingObject (selector) {
-  // gsap.to();
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
 }
+
+function floatingObject (selector, delay, size) {
+  gsap.to(selector, 
+    random(1.5, 2.5), 
+    {
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: delay
+  });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
